5655dcf67a4031561d81075d01f237f6dc4e2672
suwalak77 <kaojeed6972@gmail.com>
2023-10-07 13:53:21 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'สุวลักษณ์ ดีเลิศ',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
